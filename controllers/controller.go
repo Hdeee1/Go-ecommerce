1d33f61256104ac6e63d73231f72e4319440b325
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-10-30 08:36:48 +0700

    contloller.go: create search function

--- controllers/controller.go
+++ controllers/controller.go
@@ -131,14 +131,58 @@ func Login() gin.HandlerFunc {
 	}
 }
 
-// func ProductViewerAdmin() gin.HandlerFunc {
-	
-// }
+func ProductViewerAdmin() gin.HandlerFunc {
+	return  func(ctx *gin.Context) {
+		var product models.Product
 
-// func SearchProduct() gin.HandlerFunc {
-	
-// }
+		if err := ctx.BindJSON(&product); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return 
+		}
 
-// func SearchProductByQuery() gin.HandlerFunc {
-	
-// }
+		result := database.DB.Create(&product)
+		if result.Error != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add product"})
+			return
+		}
+
+		ctx.JSON(http.StatusCreated, gin.H{
+			"message": "Product add successfully",
+			"product_id": product.ID,
+		})
+	}
+}
+
+func SearchProduct() gin.HandlerFunc {
+	return  func(ctx *gin.Context) {
+		var products []models.Product
+
+		result := database.DB.Find(&products)
+		if result.Error != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch product"})
+			return 
+		}
+
+		ctx.JSON(http.StatusOK, products)
+	}
+}
+
+func SearchProductByQuery() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var products []models.Product
+
+		query := ctx.Query("name")
+		if query == "" {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Search query is required"})
+			return 
+		}
+
+		result := database.DB.Where("product_name LIKE ?", "%"+query+"%").Find(&products)
+		if result.Error != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Failed to search product"})
+			return 
+		}
+
+		ctx.JSON(http.StatusOK, products)
+	}
+}
