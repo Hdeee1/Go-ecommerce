b07e97eccb9aa094469da6ca7d96f0219aaa2d24
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2025-11-11 13:26:00 +0700

    bug

--- models/models.go
+++ models/models.go
@@ -12,9 +12,9 @@ type User struct {
 	gorm.Model
 	First_Name			*string			`json:"first_name" validate:"required,min=2,max=30"`
 	Last_Name			*string			`json:"last_name" validate:"required,min=2,max=30"`
-	Password			*string			`json:"password" validate:"required,min=6"`
 	Email				*string			`json:"email" validate:"required,email" gorm:"unique"`
 	Phone				*string			`json:"phone" validate:"required" gorm:"unique"`
+	Password			*string			`json:"password" validate:"required,min=6"`
 	Token				*string			`json:"token"`
 	Refresh_Token		*string			`json:"refresh_token"`
 	User_ID				string			`json:"user_id" gorm:"unique;index"`
